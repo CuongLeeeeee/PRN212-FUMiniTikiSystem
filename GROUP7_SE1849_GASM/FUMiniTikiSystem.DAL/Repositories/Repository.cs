d8d0f0ae41d249c05d2262a0807137bad9a8f089
Le Truong Cuong <lecuong27100@gmail.com>
2025-07-22 17:16:17 +0700

    feat: implement repository pattern

---
+++ GROUP7_SE1849_GASM/FUMiniTikiSystem.DAL/Repositories/Repository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FUMiniTikiSystem.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FUMiniTikiSystem.DAL.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll() =>  _dbSet.ToList();
+
+        public T? GetById(object id) =>  _dbSet.Find(id);
+
+        public  void Add(T entity) =>  _dbSet.Add(entity);
+
+        public void Update(T entity) => _dbSet.Update(entity);
+
+        public void Delete(T entity) => _dbSet.Remove(entity);
+    }
+
+}
