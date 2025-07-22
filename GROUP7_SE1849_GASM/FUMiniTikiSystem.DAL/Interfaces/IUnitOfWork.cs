ebd9014d1804745aa7f1a73546d4defa8b73c869
Le Truong Cuong <lecuong27100@gmail.com>
2025-07-22 17:24:44 +0700

    feat: add unit of work

---
+++ GROUP7_SE1849_GASM/FUMiniTikiSystem.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FUMiniTikiSystem.DAL.Entities;
+
+namespace FUMiniTikiSystem.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Product> Products { get; }
+        IRepository<Customer> Customers { get; }
+        Task<int> SaveChangesAsync();
+    }
+
+}
---
+++ GROUP7_SE1849_GASM/FUMiniTikiSystem.DAL/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FUMiniTikiSystem.DAL.Entities;
+using FUMiniTikiSystem.DAL.Interfaces;
+using FUMiniTikiSystem.DAL.Repositories;
+
+namespace FUMiniTikiSystem.DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly FuminiTikiSystemContext _context;
+        public IRepository<Product> Products { get; private set; }
+        public IRepository<Customer> Customers { get; private set; }
+
+        public UnitOfWork(FuminiTikiSystemContext context)
+        {
+            _context = context;
+            Products = new Repository<Product>(_context);
+            Customers = new Repository<Customer>(_context);
+        }
+
+        public Task<int> SaveChangesAsync() =>  _context.SaveChangesAsync();
+        public void Dispose() => _context.Dispose();
+    }
+
+}
