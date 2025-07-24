c3756bd33c06126935f305190f108420609ff578
Le Truong Cuong <lecuong27100@gmail.com>
2025-07-24 22:18:19 +0700

    feat: implement product service and repository

---
+++ GROUP7_SE1849_GASM/FUMiniTikiSystem.BLL/Interfaces/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FUMiniTikiSystem.DAL.Entities;
+
+namespace FUMiniTikiSystem.BLL.Interfaces
+{
+    public interface IProductService
+    {
+        Task<List<Product>> GetAllAsync();
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(Product product);
+    }
+}
---
+++ GROUP7_SE1849_GASM/FUMiniTikiSystem.DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FUMiniTikiSystem.DAL.Entities;
+using FUMiniTikiSystem.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FUMiniTikiSystem.DAL.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(FuminiTikiSystemContext context) : base(context)
+        {
+        }
+    }
+}
